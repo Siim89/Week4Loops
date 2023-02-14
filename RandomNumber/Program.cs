231a693ff9c4499fd7cd4d3147c94d531082184a
Your Name <you@example.com>
2023-02-14 22:30:15 +0200

    first commit

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Random rnd = new Random();
+
+int myRandomnum;
+
+int randomSum = 0;   
+
+for(int i = 0;i < 6; i++) 
+{
+    int myRandomNum = rnd.Next(0, 11);
+    randomSum= randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
